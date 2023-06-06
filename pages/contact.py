a6628b6feedca297e26fe037763f0147a53528ba
Muhammet_Cinar <mhmmtcnr81@gmail.com>
2023-06-07 01:29:24 +0300

    streamlit

---
+++ pages/contact.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.image("https://media.licdn.com/dms/image/C4D03AQGunu-NLNYGlw/profile-displayphoto-shrink_800_800/0/1567500628605?e=1689206400&v=beta&t=HOXeq_6ou85Ep0r0kc4PoOCOa1-N45ZzJHWU69T90uM")
+st.markdown("Machine Learning| Ai | Computer Vision | NLP |Big Data| Data Science |Cloud Technologies")
+st.markdown("Muhammet ÇINAR")
+st.markdown("Linkedln:https://www.linkedin.com/in/muhammetcinar/")
+st.markdown("Github: https://github.com/cinarolog")
+
+
